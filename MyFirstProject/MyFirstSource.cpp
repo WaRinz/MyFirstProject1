4fd68cc375332273e3465b15befc3883cf8c0447
WaRinz <seleniumdichloride@gmail.com>
2020-08-11 13:20:37 +0700

    weekly program1

--- MyFirstProject/MyFirstSource.cpp
+++ MyFirstProject/MyFirstSource.cpp
@@ -1,7 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	printf("test push to git hub");
-	printf("123456");
+	int age;
+	printf("Please Enter Your Age : ");
+	scanf_s("%d", &age);
+
+	if (age <= 11)
+	{
+		printf("%d -> Kid", age);
+	}
+	else if (age <= 20)
+	{
+		printf("%d -> Teen", age);
+	}
+	else if (age <= 60)
+	{
+		printf("%d -> Adult", age);
+	}
+	else
+	{
+		printf("%d -> Old", age);
+	}
 	return 0;
 }
